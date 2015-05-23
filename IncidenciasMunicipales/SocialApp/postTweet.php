b0a59bbc7f15218f23e74d0f8f51fa097d602029
Junior Zkated <tello_jr2@hotmail.com>
2015-05-23 14:47:23 -0500

    Modificacion social

--- IncidenciasMunicipales/SocialApp/postTweet.php
+++ IncidenciasMunicipales/SocialApp/postTweet.php
@@ -13,28 +13,36 @@ $accessTokenSecret = 'w0J08K3XutY9Qp2ZaIucml1oBnnWn4qcVaC6gwjrPADcE';
 $tweet = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 
 
-$url = 'http://echo.jsontest.com/key/value/one/two';
+$url = 'http://incidencia.wratix.com/api';
 $content = file_get_contents($url);
 $json = json_decode($content, true);
-print_r($json['data']);
-exit();
-foreach($json['data']['weather'] as $item) {
-    print $item['date'];
-    print ' - ';
-    print $item['weatherDesc'][0]['value'];
-    print ' - ';
-    print '<img src="' . $item['weatherIconUrl'][0]['value'] . '" border="0" alt="" />';
-    print '<br>';
-}
 
+foreach($json['results'] as $item) {
+	if(strlen($item['descripcion']) <= 120){
+		$tweetMessage = '#AlertaMunicipal ' . $item['descripcion']  ;
+	    $tweet->post('statuses/update', array('status' => $tweetMessage));
+	    echo 'Se han realizado los Twits <br>';
+	}else{
+		echo 'El Tweet supera los 140 caracteres<br>';
+	}
+	
+	// if(strlen($tweetMessage) <= 140)
+	// {
+	//     // Post the status message
+	// }
+
+	
+    
+}
+ 
 // Set status message
-$tweetMessage = 'This is a tweet to my Twitter account via PHP.';
+//$tweetMessage = 'This is a tweet to my Twitter account via PHP.';
 
 // Check for 140 characters
-if(strlen($tweetMessage) <= 140)
+/* if(strlen($tweetMessage) <= 140)
 {
     // Post the status message
     $tweet->post('statuses/update', array('status' => $tweetMessage));
 }
-
+*/
 ?>
